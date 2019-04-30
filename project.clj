23e096eb950d1fcea05c79e91cc8224b8baec14d
ishtiaq11 <ishtiaqpias@gmail.com>
2019-04-30 12:00:36 +0600

    slack request url verification

--- project.clj
+++ project.clj
@@ -4,9 +4,11 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [clj-http "3.9.1"]
-                 [http-kit "2.4.0-alpha4"]
-                 [compojure "1.6.1"]]
+                 [compojure "1.6.1"]
+                 [ring "1.7.1"]
+                 [ring/ring-json "0.4.0"]
+                 [org.julienxx/clj-slack "0.6.3"]]
   :main ^:skip-aot slackbot-vici.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :min-lein-version "2.0.0"
+  :uberjar-name "slackbot-vici.jar"
+  :profiles {:dev {:main slackbot-vici.core/-dev-main}})
--- src/slackbot_vici/core.clj
+++ src/slackbot_vici/core.clj
@@ -1,21 +1,52 @@
 (ns slackbot-vici.core
   (:gen-class)
-  (:require [org.httpkit.server :as server]
-            [compojure.core :refer :all]
-            [compojure.route :as route]))
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [compojure.core :refer [GET defroutes POST ANY]]
+            [ring.handler.dump :refer [handle-dump]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.keyword-params :refer [wrap-keyword-params]]
+            [ring.middleware.json :refer [wrap-json-params wrap-json-body wrap-json-response]]
+            [ring.util.response :refer [response]]
+            [clj-slack.chat :as chat]))
 
-(defn app [req]
-  {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    "Welcome to concitus"})
+(defn url-verification-event
+  [json-body]
+  (response {:challenge (:challenge json-body)}))
 
-(defroutes app-routes
-  (GET "/" [] app)
-  (route/not-found "404 NOT Found!"))
+(defn bot [request]
+  (println "json-body: " (:body request))
+  (let [json-body (:body request)
+        type (:type json-body)]
+    (cond
+      (= "url_verification" type)
+      (url-verification-event json-body)
+      (= "event_callback" type)
+      ; immediate http 2xx response needed
+      (response "we are processing!!!"))))
+      ; concurency needed; will implement later
+      ; (let [conn {:api-url "https://slack.com/api" :token "xoxb-620052217220-622429530583-lfn5wz3iFwfbUbiw1YLyTRac"}
+      ;       inner-type (get-in json-body [:event :type])
+      ;       channel (get-in json-body [:event :channel])]
+      ;   (println inner-type channel)
+      ;   (response "we are processing...")))))
+        ; (println (clj-slack.chat/post-message conn channel "your task is processing"))))))
+        ; (chat/post-message conn (str channel) "your task is processing...")))))
+
+
+(defroutes routes
+  ; for testing request data
+  (ANY "/request" [] (wrap-json-body handle-dump {:keywords? true}))
+  (POST "/bot" [] (wrap-json-response (wrap-json-body bot {:keywords? true}))))
+
+(def app (wrap-params routes))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (let [port (Integer/parseInt "8080")]
-    (server/run-server #'app-routes {:port port})
-    (println (str "Running webserber at http://127.0.0.1:" port "/"))))
+  [port]
+  (let [port (Integer/parseInt port)]
+    (jetty/run-jetty app {:port port})))
+
+(defn -dev-main
+  [port]
+  (let [port (Integer/parseInt port)]
+    (jetty/run-jetty (wrap-reload #'app) {:port port})))
